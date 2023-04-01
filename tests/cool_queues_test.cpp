02863351c838e940500168c110b6b5351a4b4438
stryku <stryku2393@gmail.com>
2023-04-01 13:52:51 +0200

    Added ProducerTest, Constructor

--- tests/cool_queues_test.cpp
+++ tests/cool_queues_test.cpp
@@ -14,7 +14,8 @@ TEST(BufferTest, Constructor) {
   EXPECT_EQ(header.m_end_offset, 0);
   EXPECT_EQ(header.m_footer_at_offset, 0);
   EXPECT_EQ(header.m_version, 0);
-  EXPECT_EQ(header.m_capacity, memory_buffer.size() - sizeof(header));
+  EXPECT_EQ(header.m_capacity, memory_buffer.size() - sizeof(buffer_header) -
+                                   sizeof(buffer_footer));
 
   header.m_end_offset = 40;
   header.m_footer_at_offset = 20;
@@ -25,7 +26,22 @@ TEST(BufferTest, Constructor) {
   EXPECT_EQ(header.m_end_offset, 40);
   EXPECT_EQ(header.m_footer_at_offset, 20);
   EXPECT_EQ(header.m_version, 41);
-  EXPECT_EQ(header.m_capacity, memory_buffer.size() - sizeof(header));
+  EXPECT_EQ(header.m_capacity, memory_buffer.size() - sizeof(buffer_header) -
+                                   sizeof(buffer_footer));
+}
+
+TEST(ProducerTest, Constructor) {
+  std::array<std::byte, 1024> memory_buffer;
+  producer producer{memory_buffer};
+
+  buffer buffer(memory_buffer, buffer_header{});
+  auto &header = buffer.access_header();
+  EXPECT_EQ(header.m_header_size, sizeof(header));
+  EXPECT_EQ(header.m_end_offset, 0);
+  EXPECT_EQ(header.m_footer_at_offset, 0);
+  EXPECT_EQ(header.m_version, 0);
+  EXPECT_EQ(header.m_capacity, memory_buffer.size() - sizeof(buffer_header) -
+                                   sizeof(buffer_footer));
 }
 
 } // namespace cool_q::test
