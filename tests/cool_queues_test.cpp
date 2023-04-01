a5c072c8329eea783b01fa0610e4eb83b362fe43
stryku <stryku2393@gmail.com>
2023-04-01 13:54:30 +0200

    Added ConsumerTest, Constructor

--- tests/cool_queues_test.cpp
+++ tests/cool_queues_test.cpp
@@ -44,4 +44,24 @@ TEST(ProducerTest, Constructor) {
                                    sizeof(buffer_footer));
 }
 
+TEST(ConsumerTest, Constructor) {
+  std::array<std::byte, 1024> memory_buffer;
+  producer producer{memory_buffer};
+
+  buffer buffer(memory_buffer, buffer_header{});
+  auto &header = buffer.access_header();
+  header.m_end_offset = 40;
+  header.m_footer_at_offset = 20;
+  header.m_version = 41;
+
+  consumer consumer(memory_buffer);
+  // Ensure it doesn't override header
+  EXPECT_EQ(header.m_header_size, sizeof(header));
+  EXPECT_EQ(header.m_end_offset, 40);
+  EXPECT_EQ(header.m_footer_at_offset, 20);
+  EXPECT_EQ(header.m_version, 41);
+  EXPECT_EQ(header.m_capacity, memory_buffer.size() - sizeof(buffer_header) -
+                                   sizeof(buffer_footer));
+}
+
 } // namespace cool_q::test
