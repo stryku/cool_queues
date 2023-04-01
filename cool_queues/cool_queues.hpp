90829182954d67af8b03db60858cd6c9b3943a76
stryku <stryku2393@gmail.com>
2023-04-01 10:33:37 +0200

    Initial project structure

---
+++ cool_queues/cool_queues.hpp
@@ -0,0 +1,10 @@
+
+#pragma once
+
+namespace cool_q {
+
+int foo() {
+  return 42;
+}
+
+} // namespace cool_q
---
+++ tests/cool_queues_test.cpp
@@ -0,0 +1,11 @@
+#include "cool_queues/cool_queues.hpp"
+
+#include <gtest/gtest.h>
+
+namespace cool_q::test {
+
+TEST(Foo, Bar) {
+  EXPECT_EQ(foo(), 42);
+}
+
+} // namespace cool_q::test
